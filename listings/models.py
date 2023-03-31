800f5cef44c18dce3b757830cbd579bc4ee9ddb7
Stephen Smith <stephenlehanesmith@gmail.com>
2023-03-31 19:54:15 +0100

    created realtors model

--- listings/models.py
+++ listings/models.py
@@ -12,7 +12,7 @@ class Listing(models.Model):
     description = models.TextField(blank=True)
     price = models.IntegerField()
     bedrooms = models.IntegerField()
-    bathrooms = models.DecimalField(max_digit=2, decimal_places=1)
+    bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
     price = models.IntegerField(default=0)
     sqft = models.IntegerField()
     lot_size = models.DecimalField(max_digits=5, decimal_places=1)
