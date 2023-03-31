9585bc4fa4f83eeabccd2d87c560668e1d076deb
Stephen Smith <stephenlehanesmith@gmail.com>
2023-03-31 19:43:43 +0100

    created listing model

--- re_proj/settings.py
+++ re_proj/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 're_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'redb',
+        'USER': 'insp',
+        'PASSWORD': '123456',
+        'HOST': 'localhost'
+
     }
 }
 
